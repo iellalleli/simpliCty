a3242268975e1b00960e2fa5a530147aad6fd314
Bryll Soliven <princebryll.soliven8@gmail.com>
2024-11-21 01:30:52 +0800

    Update lexers.h

--- simpliCty/lexers.h
+++ simpliCty/lexers.h
@@ -2,20 +2,28 @@
 #define LEXER_H_
 
 #include <stddef.h>
-#include <stdio.h>  // Add this line to include the FILE type
+#include <stdio.h> 
 
 typedef enum {
     TOKEN_KEYWORD,
     TOKEN_RESERVED_WORD,
     TOKEN_IDENTIFIER,
-    TOKEN_NUMBER,
+    TOKEN_INTEGER,
+    TOKEN_FLOAT,
     TOKEN_ARITHMETIC_OPERATOR,
-    TOKEN_RELATIONAL_OPERATOR,
-    TOKEN_LOGICAL_OPERATOR,
+    TOKEN_BOOLEAN_OPERATOR_RELATIONAL,  
+    TOKEN_BOOLEAN_OPERATOR_LOGICAL,     
     TOKEN_ASSIGNMENT_OPERATOR,
     TOKEN_UNARY_OPERATOR,
     TOKEN_DELIMITER,
+    TOKEN_DELIMITER_OPEN_PARENTHESIS,
+    TOKEN_DELIMITER_CLOSE_PARENTHESIS,
+    TOKEN_DELIMITER_OPEN_BRACE,
+    TOKEN_DELIMITER_CLOSE_BRACE,
+    TOKEN_DELIMITER_OPEN_BRACKET,
+    TOKEN_DELIMITER_CLOSE_BRACKET,
     TOKEN_STRING,
+    TOKEN_CHARACTER,
     TOKEN_NOISE_WORD,
     TOKEN_COMMENT,
     TOKEN_UNKNOWN,
@@ -27,7 +35,6 @@ typedef struct {
     char *value;
     size_t line_num;
 } Token;
-
 
 void print_token(const Token *token);
 Token **lexer(FILE *file, size_t *token_count);
